825ca8b25a54e21f635453941997794d7a1bc119
zhangbin <zhangbin@cocos2d-x.org>
2014-09-02 15:29:04 +0800

    Install Cocos Studio in the Mac installer of Cocos Engine.

--- tools/for-installer/installer-proj-mac/resources/postflight.sh
+++ tools/for-installer/installer-proj-mac/resources/postflight.sh
@@ -3,10 +3,12 @@
 INSATLL_DIR="/Applications/cocos"
 chmod -R a=rwx "${INSATLL_DIR}"
 
-MONO_PKG="${INSATLL_DIR}/MonoFramework.pkg"
-if [ -f ${MONO_PKG} ]; then
-    sudo installer -pkg $MONO_PKG -target /
-    rm -rf ${MONO_PKG}
+STUDIO_PKG="${INSATLL_DIR}/Cocos Studio.pkg"
+STUDIO_PATH="/Applications/Cocos Studio.app"
+if [ -f "${STUDIO_PKG}" ]; then
+    sudo installer -pkg "${STUDIO_PKG}" -target /
+    mv -f "${STUDIO_PATH}" "${INSATLL_DIR}/Cocos Studio.app"
+    rm -rf "${STUDIO_PKG}"
 fi
 
 open "${INSATLL_DIR}"
