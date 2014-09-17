d6734b3965aa32ccc8aad70e7e9d85f8b910603e
zhangbin <zhangbin@cocos2d-x.org>
2014-09-17 15:26:09 +0800

    Add android sdk, ndk & ant in the Mac installer.

--- tools/for-installer/installer-proj-mac/resources/postflight.sh
+++ tools/for-installer/installer-proj-mac/resources/postflight.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# change the premission of cocos folder
 INSATLL_DIR="/Applications/cocos"
 chmod -R a=rwx "${INSATLL_DIR}"
 
+# install cocos studio
 STUDIO_PKG="${INSATLL_DIR}/Cocos Studio.pkg"
 STUDIO_PATH="/Applications/Cocos Studio.app"
 if [ -f "${STUDIO_PKG}" ]; then
@@ -10,5 +12,9 @@ if [ -f "${STUDIO_PKG}" ]; then
     mv -f "${STUDIO_PATH}" "${INSATLL_DIR}/Cocos Studio.app"
     rm -rf "${STUDIO_PKG}"
 fi
+
+# invoke the setup.py in engine
+SETUP_PY="${INSATLL_DIR}/frameworks/cocos2d-x/setup.py"
+python "${SETUP_PY}" -a "${INSATLL_DIR}/tools/android-sdk" -n "${INSATLL_DIR}/tools/android-ndk-r9d" -t "${INSATLL_DIR}/tools/ant/bin"
 
 open "${INSATLL_DIR}"
